5bd8fb53e68cc9110958cbaa9bcbed0d0b3613f4
Maarten van Gompel <proycon@anaproy.nl>
2016-10-21 11:41:18 +0200

    Implemented reset password functionality

--- clamusers/views.py
+++ clamusers/views.py
@@ -7,6 +7,7 @@ from django.core.mail import send_mail
 from django.core.context_processors import csrf
 from django.template import RequestContext
 from django.db import IntegrityError
+import string, os, random
 import hashlib
 
 def autoactivate(clamuser):
@@ -100,6 +101,26 @@ def changepw(request, userid):
         return render_to_response('changepw.html',{'userid': userid},context_instance=c)
 
 
+def resetpw(request, userid):
+    if request.method == 'POST' and 'mail' in request.POST:
+        found = False
+        for clamuser in CLAMUsers.objects.filter(mail=request.POST['mail']):
+            found = True
+            length = 10
+            chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+            random.seed = (os.urandom(1024))
+            clamuser.password = ''.join(random.choice(chars) for i in range(length))
+            clamuser.save()
+            send_mail('Webservice account on ' + settings.DOMAIN , 'Dear ' + clamuser.fullname + '\n\nYour webservice account on ' + settings.DOMAIN + ' has had a password reset.\n\nUsername: ' + clamuser.username + '\nPassword: ' + clamuser.password + '\n\nImportant: Please change this password immediately to one of your own choosing using ' + settings.BASEURL + 'changepw/' + str(clamuser.pk)+ '\n\nIf you did not request this, please notify us immediately by replying to this message.\n\n(this is an automated message)', settings.FROMMAIL, [clamuser.mail] , fail_silently=False)
+            send_mail('[' + settings.DOMAIN + '] Password reset for ' + clamuser.username  , 'User ' + clamuser.username + ' (' + clamuser.fullname + ') forgot his credentials and executed a reset from IP ' + settings.META.get('REMOTE_ADDR') + '. This is an automated notification and no further action is required.', settings.FROMMAIL, [ x[1] for x in settings.ADMINS ] , fail_silently=False)
+        if found:
+            return HttpResponse("Done, please check your mail and follow the instructions...", content_type="text/plain")
+        else:
+            return HttpResponseForbidden("No such user exists", content_type="text/plain")
+    else:
+        c = RequestContext(request)
+        c.update(csrf(request))
+        return render_to_response('resetpw.html',context_instance=c)
 
 def userlist(request):
     if request.method == 'POST':
